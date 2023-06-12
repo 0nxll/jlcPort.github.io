fc49413fe7053af781cda3f50225d473995c0827
Jhon Luke Cutay <jhonlukecutayyy@gmail.com>
2023-06-12 12:42:34 +0800

    Add files via upload

---
+++ index-css/anim.js
@@ -0,0 +1,15 @@
+// // Use JavaScript to add a class to the element when it enters the viewport
+// document.addEventListener("scroll", function() {
+//     var element = document.querySelector(".projDiv");
+//     var position = element.getBoundingClientRect();
+  
+//     // Check if the element is inside the viewport
+//     if (position.top >= 0 && position.bottom <= window.innerHeight) {
+//       element.classList.add("fade-in") }
+//     });
+
+  
+
+
+  
+  
